0015f8b0ad1c5e3314467df0bd10edc75e84ace6
Va1Anon <va1anon@protonmail.com>
2020-12-17 09:36:29 +0100

    Reformat code, remove global var shadowing

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import os
-import gtts
-import time
 import asyncio
+import os
+import time
+from datetime import datetime
+
+import gtts
 import selenium
 import yaml
-from libroosx.utils import fetch_timetable
 from libroosx import SynAClient
 from libroosx.keychain import keychain_proxy_login
 from libroosx.librocache import CacheBridge, SQLiteCacheConnector
-from datetime import datetime
 from libroosx.synergia.mappings import SynSubject
+from libroosx.utils import fetch_timetable
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver_options = Options()
 driver_options.add_argument("start-minimized")
@@ -44,9 +45,9 @@ def log(event):
         log_file.write(log_message + "\n")
 
 
-def leave_class(driver):
+def leave_class(local_driver):
     log("Class ended")
-    driver.close()
+    local_driver.close()
 
 
 async def librus_login():
@@ -55,8 +56,8 @@ async def librus_login():
     return SynAClient(user, cache=CacheBridge(SQLiteCacheConnector(':memory:')))
 
 
-def classroom_login(driver):
-    driver.get('https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=https%3A%2F%2F'
+def classroom_login(local_driver):
+    local_driver.get('https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=https%3A%2F%2F'
                'developers.google.com%2Foauthplayground&prompt=select_account&response_type=code&client_id=407408718192'
                '.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fclassroom.announcements'
                '&access_type=offline&flowName=GeneralOAuthFlow')
@@ -64,9 +65,9 @@ def classroom_login(driver):
           "selenium)")
 
 
-async def librus_check_for_subjects(client):
+async def librus_check_for_subjects(local_client):
     subjects = {}
-    a = await client.fetch('/Subjects', syn_obj=SynSubject)
+    a = await local_client.fetch('/Subjects', syn_obj=SynSubject)
     for i in a:
         subjects[str(i)] = ''
     return subjects
@@ -75,11 +76,11 @@ async def librus_check_for_subjects(client):
 def join_classroom(lesson):
     classroom = config['classroom'][lesson]
     if classroom != '':
-        driver = webdriver.Chrome(options=driver_options)
-        driver.get('https://classroom.google.com')
+        local_driver = webdriver.Chrome(options=driver_options)
+        local_driver.get('https://classroom.google.com')
         time.sleep(5)
         try:
-            element = WebDriverWait(driver, 10).until(
+            element = WebDriverWait(local_driver, 10).until(
                 expected_conditions.presence_of_element_located((By.PARTIAL_LINK_TEXT, classroom))
             )
             log(f"Class {classroom} found")
@@ -87,33 +88,33 @@ def join_classroom(lesson):
         except TimeoutException:
             log(f"Class {classroom} not found")
         try:
-            link = WebDriverWait(driver, 10).until(
+            link = WebDriverWait(local_driver, 10).until(
                 expected_conditions.presence_of_element_located(
                     (By.XPATH, '//*[@id="yDmH0d"]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div/span/a/div'))).text
             log(f"Link found: {link}")
-            driver.get(link)
+            local_driver.get(link)
         except TimeoutException:
             log("Link not found")
         loading = True
         while loading:
             try:
-                reload_button = WebDriverWait(driver, 1).until(
+                reload_button = WebDriverWait(local_driver, 1).until(
                     expected_conditions.presence_of_element_located(
                         (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[3]/div[1]/button/div[2]')))
-                driver.refresh()
+                local_driver.refresh()
                 log("Reloading")
             except selenium.common.exceptions.TimeoutException:
                 log(f"Loaded {classroom}")
                 loading = False
         starting = True
-        camera_button = driver.find_element_by_xpath(
+        camera_button = local_driver.find_element_by_xpath(
             '//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/div/div').click()
 
-        mic_button = driver.find_element_by_xpath(
+        mic_button = local_driver.find_element_by_xpath(
             '//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[1]/div/div/div').click()
         while starting:
             try:
-                enter_meeting_button = driver.find_element_by_xpath(
+                enter_meeting_button = local_driver.find_element_by_xpath(
                     '//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div['
                     '1]/div[1]/span/span').click()
                 starting = False
@@ -161,7 +162,7 @@ while True:
     today_datetime = datetime.now()
     today_date = today_datetime.date()
     active_lesson = tuple(
-        filter(lambda x: x.hour_end >today_datetime.time() > x.hour_start,
+        filter(lambda x: x.hour_end > today_datetime.time() > x.hour_start,
                tt.auto.get(today_date.strftime('%Y-%m-%d'))))
     print(active_lesson)
     if active_lesson != active_lesson_old:
