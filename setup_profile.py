a9c584ffda744b9e17095952f297e78c51ef8a2f
Va1Anon <va1anon@protonmail.com>
2020-12-17 11:21:47 +0100

    Add config manager

---
+++ setup_profile.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+
+if os.path.exists('profile'):
+    raise Exception('usuń folder profile')
+
+from config_man import ConfingMan
+from main_driver import driver
+
+if __name__ == '__main__':
+    config = ConfingMan()
+    email, passwd = config.google_credentials
+    driver.get('https://meet.google.com')
+    login_a = driver.find_elements_by_css_selector('a[event-action="sign in"]')[0]
+    login_a.click()
+    email_box = driver.find_elements_by_css_selector('[autocomplete="username"')[0]
+    email_box.send_keys(email)
+    driver.find_elements_by_css_selector('#identifierNext')[0].click()
+    sleep(2)
+    pass_box = driver.find_elements_by_css_selector('[type="password"]')[0]
+    pass_box.send_keys(passwd)
+    driver.find_elements_by_css_selector('#passwordNext')[0].click()
+    sleep(2)
+    driver.close()
