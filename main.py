3ce35ec1d42afdc9671b661ae541168fba7a1b91
Maximilian Gaedig <maximiliangaedig@gmail.com>
2021-02-16 11:50:59 +0100

    Add translation to polish Bug fixes Make reloading more stable

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import os
 import platform
 import time
 import asyncio
 import selenium
-import yaml
 from datetime import datetime
-from win10toast import ToastNotifier
+from datetime import timedelta
 from libroosx import keychain_proxy_login, SynAClient
 from libroosx.librocache import SQLiteCacheConnector, CacheBridge
 from libroosx.utils import fetch_timetable
@@ -14,38 +12,52 @@ from selenium.common.exceptions import (
     NoSuchElementException,
     TimeoutException,
     ElementNotInteractableException,
-    ElementClickInterceptedException,
+    ElementClickInterceptedException, StaleElementReferenceException,
 )
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium import webdriver
 import chromedriver_autoinstaller
+import discord
+from discord.ext import commands
+import os
+import yaml
+
+description = '''Classroom Bot by Maximilian Gaedig'''
+# intents = discord.Intents.default()
+# global bot
+# bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+config = yaml.safe_load(open(".\config.yml", encoding='utf-8'))
 
 if platform.system() == 'windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-chromedriver_autoinstaller.install()
+chromedriver_autoinstaller.install(cwd=True)
 driver_options = Options()
-driver_options.add_argument("start-minimized")
-driver_options.add_argument("use-fake-ui-for-media-stream")
+# driver_options.add_argument("start-minimized")
 driver_options.add_experimental_option("useAutomationExtension", False)
 driver_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 driver_options.add_argument("user-data-dir=./profile")
+driver_options.add_argument("--disable-gpu")
 close_msg = "Unable to evaluate script: disconnected: not connected to DevTools\n"
-toaster = ToastNotifier()
 
 
-def log(event):
+# toaster = ToastNotifier()
+
+
+async def log(event):
     event = str(event)
     log_message = datetime.now().strftime("[%H:%M:%S] ") + event
     print(log_message)
     with open("bot.log", "a", encoding="utf-8") as log_file:
         log_file.write(log_message + "\n")
-    toaster.show_toast(datetime.now().strftime("[%H:%M:%S] "), event, duration=3)
+    config = yaml.safe_load(open("config.yml", encoding='utf-8'))
+    # channel = bot.get_channel(config["channels"]["text"]["main"])
+    # await channel.send(log_message)
 
 
-def classroom_login(config):
+async def classroom_login(config):
     if not config["logged_in"]:
         driver = webdriver.Chrome(options=driver_options)
         driver.get(
@@ -67,7 +79,7 @@ def classroom_login(config):
                     '//*[@id="submit_deny_access"]/div/button/div[2]'
                 ).click()
                 oauth_running = False
-                log("Logged in")
+                await log("Logged in")
                 config["logged_in"] = True
                 with open("config.yml", "w", encoding="utf-8") as f:
                     yaml.dump(config, f, allow_unicode=True)
@@ -76,85 +88,140 @@ def classroom_login(config):
         driver.close()
 
 
-def join_classroom(classroom):
+async def join_classroom(classroom):
     driver = webdriver.Chrome(options=driver_options)
     driver.get("https://classroom.google.com")
-    try:
-        element = WebDriverWait(driver, 30).until(
-            expected_conditions.presence_of_element_located(
-                (By.PARTIAL_LINK_TEXT, classroom)
-            )
-        )
-        log(f"Class {classroom} found")
-        element.click()
-    except TimeoutException:
-        log(f"Class {classroom} not found")
-    try:
-        print("trying")
-        link = (
-            WebDriverWait(driver, 10)
-            .until(
+    while True:
+        try:
+            element = WebDriverWait(driver, 30).until(
                 expected_conditions.presence_of_element_located(
-                    (By.PARTIAL_LINK_TEXT, "https://meet.google.com/")
+                    (By.PARTIAL_LINK_TEXT, classroom)
                 )
             )
-            .text
-        )
-        log(f"Link found: {link}")
-        driver.get(link)
-    except TimeoutException:
-        log("Link not found")
-    loading = True
-    while loading:
+            await log(f"Class {classroom} found")
+            element.click()
+            break
+        except TimeoutException:
+            await log(f"Class {classroom} not found")
+    # Getting Link
+    while True:
         try:
-            reload_button = WebDriverWait(driver, 5).until(
-                expected_conditions.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[3]/div[1]/button/div[2]',
+            link = (
+                WebDriverWait(driver, 10)
+                .until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.PARTIAL_LINK_TEXT,
+                            "https://meet.google.com/"
+                        )
                     )
                 )
-            )
+            ).text
+            await log(f"Link found: {link}")
             driver.get(link)
-            log("Reloading")
-            time.sleep(5)
-        except selenium.common.exceptions.TimeoutException:
-            log(f"Loaded {classroom}")
-            loading = False
-    starting = True
-    camera_button = driver.find_element_by_css_selector(
-        "div[data-tooltip='Turn off microphone (ctrl + d)']"
-    ).click()
-
-    mic_button = driver.find_element_by_css_selector(
-        "div[data-tooltip='Turn off camera (ctrl + e)']"
-    ).click()
-    while starting:
+            break
+        except TimeoutException:
+            await log("Link not found")
+    # Waiting for Camera Button to appear and joining
+    while True:
         try:
-            enter_meeting_button = driver.find_element_by_xpath(
-                "/html/body/div[1]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/span/span"
-            ).click()
-            starting = False
-            log(f"Class {classroom} has started")
+            if config["language"] == "pl":
+                mic_button = WebDriverWait(driver, 5).until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            'div[aria-label="Wyłącz mikrofon (Ctrl + D)"]',
+                        )
+                    )
+                ).click()
+                camera_button = WebDriverWait(driver, 5).until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            'div[aria-label="Wyłącz kamerę (Ctrl + E)"]',
+                        )
+                    )
+                ).click()
+                # Joining
+                join_btns = driver.find_elements_by_css_selector('[role="button"]')
+                while True:
+                    for btn in join_btns:
+                        if btn.text == 'Dołącz':
+                            btn.click()
+                            break
+                        elif btn.text == 'Chcę dołączyć':
+                            btn.click()
+                            break
+                    else:
+                        await asyncio.sleep(0.5)
+                        continue
+                    break
+            elif config["language"] == "en":
+                mic_button = WebDriverWait(driver, 5).until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            'div[aria-label="Turn off microphone (ctrl + d)"]',
+                        )
+                    )
+                ).click()
+                camera_button = WebDriverWait(driver, 5).until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            'div[aria-label="Turn off camera (ctrl + e)"]',
+                        )
+                    )
+                ).click()
+                # Joining
+                join_btns = driver.find_elements_by_css_selector('div[role="button"]')
+                while True:
+                    for btn in join_btns:
+                        if btn.text == 'Join now':
+                            btn.click()
+                            break
+                        elif btn.text == 'Ask to join':
+                            btn.click()
+                            break
+                    else:
+                        await asyncio.sleep(0.5)
+                        continue
+                    break
 
+            else:
+                continue
+            break
         except (
-            NoSuchElementException,
-            ElementNotInteractableException,
-            ElementClickInterceptedException,
-        ) as e:
-            print(e)
-            log(f"Waiting for class {classroom} to start...")
-            time.sleep(2)
+                NoSuchElementException,
+                ElementNotInteractableException,
+                ElementClickInterceptedException,
+                StaleElementReferenceException,
+                TimeoutException
+        ):
+            driver.get(link)
+            try:
+                error_message = WebDriverWait(driver, 5).until(
+                    expected_conditions.presence_of_element_located(
+                        (
+                            By.XPATH,
+                            '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div[1]',
+                        )
+                    )
+                ).text
+                await log(error_message)
+            except TimeoutException:
+                pass
+            await log("Reloading")
     return driver
 
 
-def leave_class(driver):
-    log("Class ended")
+async def leave_class(driver):
+    await log("Class ended")
     driver.close()
 
 
 # ---------------------------------------------------------------------------
-#                           Classroom define end
+#                           Classroom Functions end
 # --------------------------------------------------------------------------
 async def librus_login():
     config = yaml.safe_load(open("config.yml", encoding="utf-8"))
@@ -163,28 +230,32 @@ async def librus_login():
     )
     return SynAClient(user, cache=CacheBridge(SQLiteCacheConnector(":memory:")))  # Nie twórz sesji
 
+
 # --------------------------------------------------------------------------
-#                                Define end
+#                                Functions end
 # --------------------------------------------------------------------------
-async def main():
-    try:
-        os.rename("bot.log", "bot.old.log")
-    except WindowsError:
+
+if __name__ == '__main__':
+    async def main():
+        # await bot.wait_until_ready()
         try:
-            os.remove("bot.old.log")
             os.rename("bot.log", "bot.old.log")
-        except FileNotFoundError:
-            pass
-    log("Started Bot")
-    while True:
-        # First time setup
-        try:
+        except WindowsError:
+            try:
+                os.remove("bot.old.log")
+                os.rename("bot.log", "bot.old.log")
+            except FileNotFoundError:
+                pass
+        await log("Started Bot")
+        while True:
+            # First time setup
+            # try:
             # If you want to log in again change logged_in to False in the config.yml
             config = yaml.safe_load(open("config.yml", encoding="utf-8"))
             client = await librus_login()
 
             if not config["logged_in"]:
-                classroom_login(config)
+                await classroom_login(config)
             if config["classroom"] is None:
                 subjects = {}
                 a = client
@@ -217,16 +288,6 @@ async def main():
                 except AttributeError:
                     pass
                     active_lesson = None
-                if active_lesson != active_lesson_old:
-                    if active_lesson != () and active_lesson is not None:
-                        if not lesson_running:
-                            if config["classroom"][active_lesson[0].subject.name] != "":
-                                # This code runs if the lesson has changed
-                                # TODO: Check if lesson is not cancelled
-                                # Starting classroom
-                                driver = join_classroom(config["classroom"][active_lesson[0].subject.name])
-                                lesson_running = True
-                                highest_member_count = 0
                 if lesson_running:
                     try:
                         member_count = int(
@@ -244,24 +305,39 @@ async def main():
                         if member_count > highest_member_count:
                             highest_member_count = member_count
                         if highest_member_count - 5 > member_count:
-                            leave_class(driver)
+                            await leave_class(driver)
                             lesson_running = False
-                        print(member_count)
-                    except selenium.common.exceptions.TimeoutException:
+                        print(f"{member_count} people on the lesson, {highest_member_count} was the maximum")
+                    except (
+                            selenium.common.exceptions.TimeoutException,
+                            ValueError,
+                            UnboundLocalError
+                    ):
                         pass
                     except AttributeError:
                         pass
+                if active_lesson != active_lesson_old:
+                    if active_lesson != () and active_lesson is not None:
+                        if not lesson_running:
+                            await log(f"{active_lesson[0].subject.name} is the current lesson")
+                            if config["classroom"][active_lesson[0].subject.name] != "":
+                                # This code runs if the lesson has changed
+                                # TODO: Check if lesson is not cancelled
+                                # Starting classroom
+                                lesson_running = True
+                                try:
+                                    driver = await join_classroom(config["classroom"][active_lesson[0].subject.name])
+                                except Exception as e:
+                                    await log(e)
+                                highest_member_count = 0
+                            else:
+                                await log("It doesn't have a name/link in the config!")
+
                 if active_lesson is not None:
                     active_lesson_old = active_lesson
-                time.sleep(10)
-        except selenium.common.exceptions.WebDriverException as e:
-            print(e)
-            log("Browser closed")
 
-        await client.close()
-
-
-asyncio.run(main())
-
-
-
+                time.sleep(config["wait_time"])
+            await client.close()
+    asyncio.run(main())
+    # bot.loop.create_task(main())
+    # bot.run(config['token'])
